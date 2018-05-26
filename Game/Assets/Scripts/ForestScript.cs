30dcb2e9b658b589928ccc7b4841d68549b42b4e
Antonio <reytono@gmail.com>
2018-05-26 17:49:44 -0600

    --------

---
+++ Game/Assets/Scripts/ForestScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*Clase que maneja al personaje principal en niveles terrestres*/
+public class ForestScript : MonoBehaviour
+{
+
+    public float jumpForce = 200f;
+    private Rigidbody2D rb;
+    public Animator anim;
+    private float move;
+    public GameObject feet;
+    public LayerMask layerMask;
+   // AudioSource muerteS;
+    public float speed = 50f;
+    AudioSource saltoS;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        //muerteS = GetComponent<AudioSource>();
+       saltoS = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        move = Input.GetAxis("Horizontal");
+
+        //anim.SetFloat("Speed", Mathf.Abs(move)*speed);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            rb.velocity = Vector2.zero;
+            rb.AddForce(Vector2.up * jumpForce);
+            saltoS.Play();
+            
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameController.instance.score++;
+        Debug.Log(GameController.instance.score);
+        GameController.instance.gameOver = true;
+       // muerteS.Play();
+       
+        
+
+
+    }
+
+    
+
+}
---
+++ Game/Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*Clae con gameCotroller*/
+public class GameController : MonoBehaviour {
+    public bool gameOver = false;
+    public int score = 0;
+    public static GameController instance;
+    
+
+	// Use this for initialization
+	void Start () {
+        instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
