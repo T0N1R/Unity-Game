b8a10b97d5713aa3c8a5fcaacb506525c94c9fb7
Antonio <reytono@gmail.com>
2018-05-26 20:43:47 -0600

    update

---
+++ Game/Assets/Scripts/Column.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*Clase control de enimgos*/
+public class ControlEnemy : MonoBehaviour {
+
+    public float scrollingSpeed = 8f;
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+        if(GameController.instance.gameOver != true)
+        {
+            transform.Translate(Vector3.left * scrollingSpeed * Time.deltaTime);
+            if (transform.position.x < -10)
+            {
+                Destroy(gameObject);
+            }
+        }
+        
+    }
+}
