5282353b0a4e65037662d2aa3a8fef7728db81e5
Antonio <reytono@gmail.com>
2018-05-26 21:08:49 -0600

    ----

---
+++ Game/Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ * Control del movimiento del fondo*/
+public class ScrollingBackground : MonoBehaviour {
+
+     float scrollingSpeed = 8f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(GameController.instance.gameOver != true)
+        {
+            transform.Translate(new Vector3(-1, 0, 0) * scrollingSpeed * Time.deltaTime);
+            if (transform.position.x < -20.4f)
+            {
+                transform.position = new Vector3(20.4f, transform.position.y, transform.position.z);
+            }
+        }
+        
+
+    }
+}
